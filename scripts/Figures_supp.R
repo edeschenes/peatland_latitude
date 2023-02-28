ff3f8869c43a42f766bb98e11d843c7bd2dee803
Elise Deschenes <68603573+edeschenes@users.noreply.github.com>
2023-02-27 21:49:06 -0500

    Update Figures_supp.R

--- scripts/Figures_supp.R
+++ scripts/Figures_supp.R
@@ -10,6 +10,8 @@ require(gridExtra)
 library(reshape2)
 require(ggpmisc)
 
+set_theme(base = theme_classic()) 
+
 #Load data
 #Site x environmental variables matrix
 sites <- read.csv("Data_repository/sites_data.csv")
@@ -24,7 +26,7 @@ bryo <- data.frame(bryo[,-1], row.names = bryo$X)
 sites <- data.frame(sites, row.names = sites$ID_Quadrat)
 
 #Select only environmental variables used in the supplementary analyses (Habitat, latitude, longitude, mean annual temperature, mean annual precipitations, peat thickness, surface water)
-env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water)
+env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum)
 
 #Assign habitat as factor
 env$Habitat=as.factor(env$Habitat)
@@ -47,7 +49,7 @@ bog_bry <- bog_bry[,colSums(bog_bry != 0) > 0]
 #Create environmental dataframe with only bogs
 env_bog<- env[row.names(env) %in% quad_bog,] #select bogs in site x enviro matrix
 #Scale and center environmental variables
-env_bog[,c(2:7)] <- as.data.frame(scale(env_bog[,c(2:7)], center = TRUE, scale = TRUE))
+env_bog[,c(2:8)] <- as.data.frame(scale(env_bog[,c(2:8)], center = TRUE, scale = TRUE))
 
 ####Fens ####
 fen <- sites[sites[, 'Habitat'] == 'Fen', ] #select fens
@@ -59,7 +61,7 @@ fen_bry <- bryo[row.names(bryo) %in% quad_fen,] #select fens in site x moss abun
 #Create environmental dataframe with only bogs
 env_fen<- env[row.names(env) %in% quad_fen,]#select fens in site x enviro matrix
 #Scale and center environmental variables
-env_fen[,c(2:7)] <- as.data.frame(scale(env_fen[,c(2:7)], center = TRUE, scale = TRUE))
+env_fen[,c(2:8)] <- as.data.frame(scale(env_fen[,c(2:8)], center = TRUE, scale = TRUE))
 
 
 ##################################################
@@ -81,7 +83,7 @@ pointvec <- c(19,17)
 sites$Habitat=as.factor(sites$Habitat)
 
 #Plot of PCA of bioclimatic variables
-svglite("figure_S1_pca_bioclim.svg")
+svglite("figure_S1_pca_bioclim.svg", width = 6.6, fix_text_size = FALSE, pointsize =8)
 plot(env.pca, scaling=2, type="none", xlab=c("PCA1 (47.73%)"), ylab=c("PCA2 (31.03%)"),xlim=c(-2,3), ylim=c(-1.5, 2))
 points(scores(env.pca, display="sites", choices=c(1,2), scaling=2),pch=21, col="black", bg="black", cex=0.7)
 with(sites, points(env.pca, display = "sites", col = colvec[Habitat],
@@ -98,21 +100,112 @@ text(scores(env.pca, display="species", choices=c(1), scaling=2),
      col="black", cex=1)   
 dev.off()
 
+
+####################################
+#### Figure S2 - PCOA BOG / FEN ####
+####################################
+#Betadisper: multivariate dispersion analysis
+#permutest.betadisper: comparison of group mean dispersions
+#rda and anova.cca: tests of centroid locations
+bry.bray <- vegdist(bryo)
+vas.bray <- vegdist(vascu)
+
+gr <- env$Habitat
+
+b.disp.bry <- betadisper(bry.bray, group = gr, type = c("centroid"), bias.adjust = FALSE)
+b.disp.vas <- betadisper(vas.bray, group = gr, type = c("centroid"), bias.adjust = FALSE)
+
+## S3 method for class 'betadisper'
+anova(b.disp.bry) 
+anova(b.disp.vas) 
+
+## Permutation test for F
+permutest(b.disp.bry, pairwise = TRUE, permutations = 9999) #Permdisp: p = 0.01
+permutest(b.disp.vas, pairwise = TRUE, permutations = 9999) #Permdisp: p = 0.01
+
+####Test of centroid locations 
+adonis2(bry.bray ~ Habitat, data=env, permutations=9999) 
+adonis2(vas.bray ~ Habitat, data=env, permutations=9999)
+
+#Download PCoA biplots 
+svglite("figureS2_pcoa.svg",  width = 6.6, fix_text_size = FALSE, pointsize =8)
+par(mfrow=c(1,2))
+plot(b.disp.vas, ellipse = TRUE, hull = FALSE, col = c('lightcoral','lightblue3')) 
+plot(b.disp.bry, ellipse = TRUE, hull = FALSE,col = c('lightcoral','lightblue3')) 
+dev.off()
+
+#Distance to centroid figures. Added manually in inkscape to the PCoA plots
+svglite("figureS2_distcent.svg")
+par(mfrow=c(1,2))
+boxplot(b.disp.vas, ylab = "Distance to centroid", main = 'Vascular',col = c("lightcoral", "lightblue3","lightcoral", "lightblue3","lightcoral", "lightblue3"), cex.axis =1.5 ,cex.lab=1.5)
+boxplot(b.disp.bry, ylab = "Distance to centroid", main = 'Bryophyte',col =  c("lightcoral", "lightblue3","lightcoral", "lightblue3","lightcoral", "lightblue3"), cex.axis = 1.5,cex.lab=1.5)
+dev.off()
+
+################################################
+#### Figure S3 - PCA of species composition ####
+################################################
+#Run PCA of species
+vas.hel <- (decostand(vascu, "hellinger"))
+bry.hel <- (decostand(bryo, "hellinger"))
+
+vas.pca <- rda(vas.hel) 
+summary(vas.pca)	# Scaling 2 (default) 
+
+bry.pca <- rda(bry.hel) 
+summary(bry.pca)	# Scaling 2 (default) 
+
+#Prep for PCA plot
+scl <- 2 ## scaling == 3
+colvec <- c('lightcoral','lightblue3')
+pointvec <- c(19,17)
+sites$Habitat=as.factor(sites$Habitat)
+
+svglite("figure_S3_pca_species.svg",  width = 6.6, fix_text_size = FALSE, pointsize =8)
+par(mfrow=c(2,1))
+plot(vas.pca, scaling=2,, type="none", xlab=c("PCA1 (22.25%)"), ylab=c("PCA2 (11.51%)"))
+with(env, points(vas.pca, display = "sites", col = colvec[Habitat],
+                 scaling = scl, pch = pointvec[Habitat], bg = colvec[Habitat]))
+with(env, legend("topright", legend = levels(Habitat), bty = "n",
+                 col = colvec, pch = pointvec[Habitat], pt.bg = colvec))
+arrows(0,0,
+       scores(vas.pca, display="species", choices=c(1), scaling=2),
+       scores(vas.pca, display="species", choices=c(2), scaling=2),
+       col="black",length=0)
+text(scores(vas.pca, display="species", choices=c(1), scaling=2),
+     scores(vas.pca, display="species", choices=c(2), scaling=2),
+     labels=rownames(scores(vas.pca, display="species", scaling=2)),
+     col="black", cex=1)  
+
+plot(bry.pca, scaling=2, type="none", xlab=c("PCA1 (18.16%)"), ylab=c("PCA2 (10.80%)"))
+with(env, points(bry.pca, display = "sites", col = colvec[Habitat],
+                 scaling = scl, pch = pointvec[Habitat], bg = colvec[Habitat]))
+with(env, legend("topright", legend = levels(Habitat), bty = "n",
+                 col = colvec, pch = pointvec[Habitat], pt.bg = colvec))
+arrows(0,0,
+       scores(bry.pca, display="species", choices=c(1), scaling=2),
+       scores(bry.pca, display="species", choices=c(2), scaling=2),
+       col="black",length=0)
+text(scores(bry.pca, display="species", choices=c(1), scaling=2),
+     scores(bry.pca, display="species", choices=c(2), scaling=2),
+     labels=rownames(scores(bry.pca, display="species", scaling=2)),
+     col="black", cex=1)   
+dev.off()
+
 #################################################################
-#### Figure S2 - Correlation plot of environmental variables ####
+#### Figure S4 - Correlation plot of environmental variables ####
 #################################################################
 
 #Select environmental variables in bogs
-env_bog<- dplyr::select(env_bog, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water)
+env_bog<- dplyr::select(env_bog, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum)
 
 #Change column names for plotting
-colnames(env_bog) <- c("Latitude",'Longitude', 'Annual mean temperature', 'Annual precipitation','Peat thickness', 'Surface water')
+colnames(env_bog) <- c("Latitude",'Longitude', 'Annual mean temperature', 'Annual precipitation','Peat thickness', 'Surface water', 'Substratum')
 
 #Select environmental variables in fens
-env_fen<- dplyr::select(env_fen, Latitude,Longitude, bioclim_1, bioclim_12, Thickness, Surface_water)
+env_fen<- dplyr::select(env_fen, Latitude,Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum)
 
 #Change column names for plotting
-colnames(env_fen) <- c("Latitude",'Longitude', 'Annual mean temperature', 'Annual precipitation','Peat thickness', 'Surface water')
+colnames(env_fen) <- c("Latitude",'Longitude', 'Annual mean temperature', 'Annual precipitation','Peat thickness', 'Surface water', 'Substratum')
 
 #Calculate Pearson correlations between each pair of environmental variables
 r_bog <- cor(env_bog, use="complete.obs")
@@ -123,15 +216,16 @@ cor_bog <- ggcorr(env_bog, method = c("pairwise", "pearson"), label = TRUE, hjus
 cor_fen <- ggcorr(env_fen, method = c("pairwise", "pearson"), label = TRUE, hjust = 0.75)
 
 ####Download SVG file for modification in Inkscape ####
-svglite("figure_S2_corplot.svg")
+svglite("figure_S2_corplot.svg", width = 6.6, fix_text_size = FALSE, pointsize =8)
 gridExtra::grid.arrange(cor_bog, cor_fen, ncol=2)
 dev.off()
 
+
 ##############################################################
-#### Figure S3 - Environmental variables in bogs and fens ####
+#### Figure S5 - Environmental variables in bogs and fens ####
 ##############################################################
 #Select environmental variables
-env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water)
+env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum)
 
 ####Plot each variable as a functional of latitude, separately for bogs and fens 
 
@@ -159,7 +253,7 @@ p2 <-ggplot(data= env, aes(Habitat, Longitude))+
         legend.text=element_text(size=12))
 
 fm1 <- aov(Longitude~Habitat, data=env)#run anova to determine difference between bogs and fens. 
-anova(fm1) #P-values were used to determine significatn differences between bogs and fens, and letters were added in Inkscape.
+anova(fm1) #P-values were used to determine significant differences between bogs and fens, and letters were added in Inkscape.
 
 p3 <-ggplot(data= env, aes(Habitat, bioclim_1))+
   stat_boxplot( aes(Habitat, bioclim_1), 
@@ -172,7 +266,7 @@ p3 <-ggplot(data= env, aes(Habitat, bioclim_1))+
         legend.text=element_text(size=12))
 
 fm1 <- aov(bioclim_1~Habitat, data=env)#run anova to determine difference between bogs and fens. 
-anova(fm1) #P-values were used to determine significatn differences between bogs and fens, and letters were added in Inkscape.
+anova(fm1) #P-values were used to determine significant differences between bogs and fens, and letters were added in Inkscape.
 
 p4<-ggplot(data= env, aes(Habitat, bioclim_12))+
   stat_boxplot( aes(Habitat, bioclim_12), 
@@ -185,7 +279,7 @@ p4<-ggplot(data= env, aes(Habitat, bioclim_12))+
         legend.text=element_text(size=12))
 
 fm1 <- aov(bioclim_12~Habitat, data=env)#run anova to determine difference between bogs and fens. 
-anova(fm1) #P-values were used to determine significatn differences between bogs and fens, and letters were added in Inkscape.
+anova(fm1) #P-values were used to determine significant differences between bogs and fens, and letters were added in Inkscape.
 
 p5<-ggplot(data= env, aes(Habitat, Thickness))+
   stat_boxplot( aes(Habitat, Thickness), 
@@ -213,19 +307,30 @@ p6<-ggplot(data= env, aes(Habitat, Surface_water))+
 fm1 <- aov(Surface_water~Habitat, data=env)#run anova to determine difference between bogs and fens. 
 anova(fm1) #P-values were used to determine significatn differences between bogs and fens, and letters were added in Inkscape.
 
+p7<-ggplot(data= env, aes(Habitat, Substratum))+
+  stat_boxplot( aes(Habitat, Deposit), 
+                geom='errorbar', linetype=1, width=0.5)+  #whiskers
+  geom_boxplot( aes(Habitat, Substratum),outlier.shape=1) +    
+  stat_summary(fun=mean, geom="point", size=3)+
+  labs(y="Substratum", x = "Habitat")+
+  theme(axis.title = element_text(size = 12),
+        axis.text=element_text(size=12), 
+        legend.text=element_text(size=12))
+
+fm1 <- aov(Substratum~Habitat, data=env)#run anova to determine difference between bogs and fens. 
+anova(fm1) #P-values were used to determine significatn differences between bogs and fens, and letters were added in Inkscape.
+
 ####Download SVG file for modification in Inkscape ####
-svglite("figure_S3_env.svg", width = 14, height = 7)
-grid.arrange(p1,p3,p5,p2,p4,p6,ncol=3)
+svglite("figure_S3_env.svg", width = 6, height = 12)
+grid.arrange(p1,p3,p5,p2,p4,p6, p7, ncol=2)
 dev.off()
 
 ############################################
-####Figure S4. PCA of functional traits ####
+####Figure S6. PCA of functional traits ####
 ############################################
 #PCA is conducted on the site x trait class frequencies matrix.
 #Step 1. Calculate trait class frequencies with function functcomp
 #Step 2. PCA on Hellinger transformed site x trait class frequencies matrix
-
-####
 
 #Open vascular trait data 
 traits_vasc <- read.csv("Data_repository/traits_vasc_data.csv",sep = ";", header= TRUE)
@@ -308,7 +413,6 @@ colvec <- c('lightcoral','lightblue3')
 pointvec <- c(19,17)
 sites$Habitat=as.factor(sites$Habitat)
 
-
 ####################################################
 #### Plot of vascular species functional traits ####
 ####################################################
@@ -317,9 +421,14 @@ traits_vasc_hel <- sqrt(cwm_vascu) #Hellinger transformation on site x trait cla
 vas.funct.pca <- rda(traits_vasc_hel) #perform PCA for vascular traits
 summary(vas.funct.pca)
 
+traits_bryo_hel <- sqrt(cwm_bryo)#Hellinger transformation on site x trait class frequencies
+bry.funct.pca <- rda(traits_bryo_hel) #perform PCA for moss traits
+summary(bry.funct.pca)
+
 #Plot
-svglite("figure_S4_pca_vasc.svg")
-plot(vas.funct.pca, scaling=2, main="PCA - vascular traits", type="none", xlab=c("PCA1 (56.02%)"), ylab=c("PCA2 (12.92%)"), xlim=c(-1.3, 1.3), ylim=c(-1,1), cex =1)
+svglite("figure_S6_pca_traits.svg", width= 6.6, fix_text_size = FALSE, pointsize =8.5)
+par(mfrow = c(2,1))
+plot(vas.funct.pca, scaling=2, type="none", xlab=c("PCA1 (56.02%)"), ylab=c("PCA2 (12.92%)"), xlim=c(-1.3, 1.3), ylim=c(-1,1), cex =1)
 with(env, points(vas.funct.pca, display = "sites", col = colvec[Habitat],
                  scaling = scl, pch = pointvec[Habitat], bg = colvec[Habitat]))
 with(env, legend("topright", legend = levels(Habitat), bty = "n",
@@ -331,21 +440,8 @@ arrows(0,0,
 text(scores(vas.funct.pca, display="species", choices=c(1), scaling=2)*1.1,
      scores(vas.funct.pca, display="species", choices=c(2), scaling=2)*1.1,
      labels=rownames(scores(vas.funct.pca, display="species", scaling=2)),
-     col="black", cex=1)   
-dev.off()
-
-
-################################################
-#### Plot of moss species functional traits ####
-################################################
-
-traits_bryo_hel <- sqrt(cwm_bryo)#Hellinger transformation on site x trait class frequencies
-bry.funct.pca <- rda(traits_bryo_hel) #perform PCA for moss traits
-summary(bry.funct.pca)
-
-#Plot
-svglite("figure_S4_pca_bryo.svg")
-plot(bry.funct.pca, scaling=2, main="PCA - bryo traits", type="none", xlab=c("PCA1 (41.21%)"), ylab=c("PCA2 (20%)"), xlim=c(-1.3, 1.3), ylim=c(-1.35,1.35), cex = 1)
+     col="black", cex=1)  
+plot(bry.funct.pca, scaling=2,type="none", xlab=c("PCA1 (41.26%)"), ylab=c("PCA2 (19.92%)"), xlim=c(-1.3, 1.3), ylim=c(-1.5,1.5), cex = 1)
 with(env, points(bry.funct.pca, display = "sites", col = colvec[Habitat],
                  scaling = scl, pch = pointvec[Habitat], bg = colvec[Habitat]))
 with(env, legend("topright", legend = levels(Habitat), bty = "n",
@@ -363,7 +459,7 @@ dev.off()
 ############################################################################
 ####Figure S5. Functional trait frequencies along latitudinal gradients ####
 ############################################################################
-env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water) #select environmental data
+env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum) #select environmental data
 
 my.formula <- y ~ x #formula used for plotting of trait frequencies as a function of latitude (linear)
 
@@ -371,7 +467,6 @@ my.formula <- y ~ x #formula used for plotting of trait frequencies as a functio
 #### Vasc traits - latitude ####
 ################################
 env<-merge(env, cwm_vascu, by = "row.names") #merge trait frequencies (CWM matrix) with environmental data for easy plotting. 
-
 
 env.long <- melt(env, id = c("Row.names", 'Latitude'), measure = c('Herbaceous','Shrub','Tree'), value.name = 'CWM_life')
 
@@ -385,7 +480,6 @@ g.1 <- ggplot(env.long, aes(x=Latitude, y=CWM_life, color=variable)) +
   coord_cartesian(ylim=c(0,0.8))
 g.1
 
-
 env.long <- melt(env, id = c("Row.names", 'Latitude'), measure = c('Height 1','Height 2','Height 3'), value.name = 'CWM_height')
 
 g.2 <- ggplot(env.long, aes(x=Latitude, y=CWM_height, color=variable)) +
@@ -397,7 +491,6 @@ g.2 <- ggplot(env.long, aes(x=Latitude, y=CWM_height, color=variable)) +
   theme(legend.title = element_blank())+
   coord_cartesian(ylim=c(0,0.8))
 g.2
-
 
 env.long <- melt(env, id = c("Row.names", 'Latitude'), measure = c('Seed 1','Seed 2','Seed 3'), value.name = 'CWM_Seed')
 
@@ -424,7 +517,7 @@ g.4 <- ggplot(env.long, aes(x=Latitude, y=CWM_SLA, color=variable)) +
 g.4
 
 ####Download SVG file for modification in Inkscape ####
-svglite("figure_S5_vasc_traits.svg")
+svglite("figure_S7_vasc_traits.svg", , width= 6.6, fix_text_size = FALSE, pointsize =8.5)
 grid.arrange(g.1, g.2, g.3, g.4,ncol=2)
 dev.off()
 
@@ -432,7 +525,7 @@ dev.off()
 #### Moss traits - latitude ####
 ################################
 
-env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water)
+env <- dplyr::select(sites, Habitat, Latitude, Longitude, bioclim_1, bioclim_12, Thickness, Surface_water, Substratum)
 env<-merge(env, cwm_bryo, by = "row.names")
 
 env.long <- melt(env, id = c("Row.names", 'Latitude'), measure = c('Acrocarpous','Pleurocarpous','Sphagnum'), value.name = 'CWM_group')
@@ -446,6 +539,17 @@ g.1 <- ggplot(env.long, aes(x=Latitude, y=CWM_group, color=variable)) +
   theme(legend.title = element_blank())+
   coord_cartesian(ylim=c(0,1.1))
 g.1
+
+###stat_poly_eq not working for bryophyte.group. We manually calculated the values using lm, since that is what stat_poly_eq does. 
+acrocarpous <- env.long[env.long$variable == 'Acrocarpous', ]
+pleurocarpous <- env.long[env.long$variable == 'Pleurocarpous', ]
+sphagnum <- env.long[env.long$variable == 'Sphagnum', ]
+
+summary(lm(CWM_group ~ Latitude, data = acrocarpous))
+summary(lm(CWM_group ~ Latitude, data = pleurocarpous))
+summary(lm(CWM_group ~ Latitude, data = sphagnum))
+#These values were manually added to the plot in inkscape.
+####
 
 env.long <- melt(env, id = c("Row.names", 'Latitude'), measure = c('Turf','Tuft','Weft'), value.name = 'CWM_growth')
 
@@ -533,6 +637,6 @@ g.8
 
 
 ####Download SVG file for modification in Inkscape ####
-svglite("figure_S6_bry_traits.svg", width = 7, height = 10)
+svglite("figure_S8_bry_traits.svg", width= 6.6,height = 9, fix_text_size = FALSE, pointsize =8.5)
 grid.arrange(g.1, g.2, g.3, g.4,g.5,g.6,g.7,g.8,ncol=2)
 dev.off()
